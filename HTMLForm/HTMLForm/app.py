e68a6a949a2caab65bb8d5b94b5dfedbb013014f
cmelendez96 <54824725+cmelendez96@users.noreply.github.com>
2020-07-18 02:02:22 -0700

    Add files via upload

---
+++ HTMLForm/HTMLForm/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("form.html")
+
+
+@app.route("/review", methods=['GET', 'POST'])
+def process_data():
+    if request.method == 'POST':
+        print("-----------------------------------------")
+        print(request.form)
+        data = request.form
+        address_list = [data['street'], data['city'] ,data['state'], data['country'], data['area']]
+        address = " ".join(address_list)
+        address_q = "+".join(address.split())
+
+        print(address)
+        print(address_q)
+        return render_template("confirmationpage.html", result=data, address_q=address_q)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port=5000)
